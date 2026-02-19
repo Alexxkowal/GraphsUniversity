3d29c52f4194dada159e34137c1393404f9524d0
Alexxkowal <alexxkowal@mail.ru>
2026-02-19 12:34:44 +0400

    fixed single node login in texteserializer class

--- src/main/java/com/graphs/utils/TextGraphSerializer.java
+++ src/main/java/com/graphs/utils/TextGraphSerializer.java
@@ -18,6 +18,7 @@ public class TextGraphSerializer<T> implements GraphSerializer<T> {
             String className = graph instanceof DirectedGraph<T> ? "directed" : "undirected";
             String graphType = graph.isWeighted() ? "weighted" : "unweighted";
             fileWriter.write(className + " " + graphType + System.lineSeparator());
+            Set<Node<T>> nodeSet = new HashSet<>();
             for (Node<T> node : graph.getNodes()) {
                 for (Edge<T> edge : graph.getEdges(node)) {
                     String s;
@@ -31,8 +32,15 @@ public class TextGraphSerializer<T> implements GraphSerializer<T> {
                     } else {
                         s = edge.getStartNode() + " " + edge.getEndNode() + System.lineSeparator();
                     }
+                    nodeSet.add(edge.getStartNode());
+                    nodeSet.add(edge.getEndNode());
                     fileWriter.write(s);
                 }
+            }
+            Set<Node<T>> remainderNodes = new HashSet<>(graph.getNodes());
+            remainderNodes.removeAll(nodeSet);
+            for(Node<T> node: remainderNodes){
+                fileWriter.write(node.toString() + System.lineSeparator());
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -59,6 +67,11 @@ public class TextGraphSerializer<T> implements GraphSerializer<T> {
             while ((line = bufferedReader.readLine()) != null) {
                 if (line.trim().isEmpty()) continue;
                 parts = List.of(line.split(" "));
+                if (parts.size() == 1){
+                    firstNode = nodeCache.computeIfAbsent(parts.getFirst(), name -> (Node<T>) new Node<>(name));
+                    graph.addNode(firstNode);
+                    continue;
+                }
                 firstNode = nodeCache.computeIfAbsent(parts.getFirst(), name -> (Node<T>) new Node<>(name));
                 secondNode = nodeCache.computeIfAbsent(parts.get(1), name -> (Node<T>) new Node<>(name));
                 weight = isWeighted?Integer.parseInt(parts.get(2)):1;
--- src/test/java/com/graphs/utils/TextGraphSerializerTest.java
+++ src/test/java/com/graphs/utils/TextGraphSerializerTest.java
@@ -13,7 +13,13 @@ class TextGraphSerializerTest {
     void testExportAndImport() {
         DirectedGraph<String> directedGraph = new DirectedGraph<>(true);
         TextGraphSerializer<String> textGraphSerializer = new TextGraphSerializer<>();
+        directedGraph.addNode(new Node<>("T"));
         directedGraph.addEdge(new Node<>("A"), new Node<>("B"), 5);
+        directedGraph.addEdge(new Node<>("B"), new Node<>("C"), 2);
+        directedGraph.addEdge(new Node<>("C"), new Node<>("A"), 3);
+        directedGraph.addEdge(new Node<>("D"), new Node<>("F"), 4);
+        directedGraph.addEdge(new Node<>("G"), new Node<>("G"), 5);
+        directedGraph.addNode(new Node<>("H"));
         textGraphSerializer.exportGraph(directedGraph, "test.txt");
         DirectedGraph<String> directedGraph2 = new DirectedGraph<>(true);
         textGraphSerializer.importGraph(directedGraph2, "test.txt");
